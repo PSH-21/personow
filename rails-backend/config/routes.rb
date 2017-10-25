ccb3b30a23da28990fede02a6158e4b0f0790ca1
PSH <peter.s.hunt@gmail.com>
2017-10-25 16:58:57 -0700

    added api routes

--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   namespace :api do
+
+  namespace :api do
     namespace :v1 do
       resources :users
+      resources :events
     end
   end
 end
