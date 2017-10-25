cf949a81d9784a8ac9b8b7aacf925b02d47b8fbd
Matt Latimer <matt.latimer@gmail.com>
2017-10-25 15:27:19 -0700

    path fix

--- webpack-frontend/index.jsx
+++ webpack-frontend/index.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './app.jsx';
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './src/routes.jsx'
