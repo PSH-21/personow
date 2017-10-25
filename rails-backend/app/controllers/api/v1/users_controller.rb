a722e1ecbcd76fd54adb9e9f70e5b10eb47c94b0
PSH <peter.s.hunt@gmail.com>
2017-10-25 15:43:41 -0700

    added api routes & api user controller

---
+++ rails-backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+module Api::V1
+  class UsersController < ApplicationController
+    def index
+      @users = User.all
+      render json: @users
+    end
+
+    def show
+      # @user = User.find(params[:id])
+      # render json: @user
+      respond_with User.find(params[:id])
+  end
+end
