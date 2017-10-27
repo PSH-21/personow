848e6fbbf00575cdc6ab85cd2347b0d209df9e17
Matt Latimer <matt.latimer@gmail.com>
2017-10-26 19:52:41 -0700

    register page creates users.

--- rails-backend/app/controllers/api/v1/users_controller.rb
+++ rails-backend/app/controllers/api/v1/users_controller.rb
@@ -13,18 +13,18 @@ module API::V1
 
     def create
       user = User.new(user_params)
-      if user.save
-        session[:user_id] = user.id
-        redirect_to '/'
+      if user.save!
+        p 'user saved'
       else
-        redirect_to '/signup'
+        p 'not saved'
       end
-
     end
+
+    
 
     private
     def user_params
-      params.require(:user).permit(
+      params.permit(
         :name,
         :email,
         :password,
