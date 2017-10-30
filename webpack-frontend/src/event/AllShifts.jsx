620e734f05cea8d50521b12a78f53cbb6a0c9ab8
Peter Hunt <peter.s.hunt@gmail.com>
2017-10-30 00:30:32 +0000

    add in commented code for AllShifts

--- webpack-frontend/src/event/AllShifts.jsx
+++ webpack-frontend/src/event/AllShifts.jsx
@@ -18,6 +18,24 @@ export default class AllShifts extends Component {
     return (
       <div>
 
+        // {
+          // !!allshifts.length ?
+          //   {
+          //     allshifts.map(shift => {
+          //       return (
+          //         <ul>
+          //           <li>{shift.date}
+          //             <ul>
+          //               <li>{shift.role_name}, {shift.start_time} - {shift.end_time}, {shift.user_name}, cancel_shift</li>
+          //             </ul>
+          //           </li>
+          //         </ul>
+          //       )
+          //     })
+          //   } :
+          // <div>Loading</div>
+        // }
+        // {error && <div>{error}</div>}
       </div>
     );
   }
