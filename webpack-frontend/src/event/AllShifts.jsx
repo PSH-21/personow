4c7241769c35f9250b9e48776a87a16a2d141c75
Peter Hunt <peter.s.hunt@gmail.com>
2017-11-01 01:41:58 +0000

    map over date structure for allshifts

--- webpack-frontend/src/event/AllShifts.jsx
+++ webpack-frontend/src/event/AllShifts.jsx
@@ -5,6 +5,11 @@ import moment from 'moment';
 
 
 export default class AllShifts extends Component {
+    static defaultProps = {
+      allshifts: [],
+      creator: '',
+      error: ''
+  }
   // static PropTypes = {
   //  shifts: PropTypes.array
   // }
@@ -30,7 +35,7 @@ export default class AllShifts extends Component {
   //     this.setState({ error })
   //   })
   // }
-  deleteOnClikc = (id, e) => {
+  deleteOnClick = (id, e) => {
     e.preventDefault();
     const shift_id = id;
     const token = localStorage.getItem('token');
@@ -52,62 +57,100 @@ export default class AllShifts extends Component {
     })
   }
 
+// [
+//   {
+//     end_time: "2017-11-02T17:00:00.000Z"
+//     event_id: 1
+//     id: 1
+//     role_id: 4
+//     role_name: "MC"
+//     start_time: "2017-11-02T13:00:00.000Z"
+//     user_id: 2
+//     user_name: "Bob McRobert"
+//   },
+//   {
+//     end_time: "2017-11-02T17:00:00.000Z"
+//     event_id: 1
+//     id: 1
+//     role_id: 4
+//     role_name: "MC"
+//     start_time: "2017-11-02T13:00:00.000Z"
+//     user_id: 2
+//     user_name: "Bob McRobert"
+//   }
+// ]
+
+
 
 
   render() {
-    const { allshifts = [], creator= '', error= '' } = this.props;
-    const order_shifts = {};
+    const { allshifts, creator, error} = this.props;
+
+    console.log(allshifts);
+    const days = [
     {
-      !!allshifts.length ?
-      (
-        for (let i = 0; i < allshifts.length; i++) {
-          if (!order_shifts.allshifts[i]['date']) {
-            order_shifts.allshifts[i]['date'] = []
-          }
-          order_shifts.allshifts[i]['date'].push(allshifts[i])
-        }
-        console.log(order_shifts);
-      ) : ''
+      date: 'oct 1',
+      shifts: [
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' },
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' }
+      ]
+    },
+    {
+      date: 'oct 2',
+      shifts: [
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' },
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' }
+      ]
+    },
+    {
+      date: 'oct 3',
+      shifts: [
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' },
+        { start: '2017-11-02T13:00:00.000Z', end: '2017-11-02T17:00:00.000Z', role_name: 'test', id: 1, user_name: 'name' }
+      ]
     }
+  ]
+
+    const shifts = days.map(day => {
+      return (
+        <ul>
+          <li>{day.date}
+            <ul>
+              { day.shifts.map(shift => {
+                return (
+                  <li key={shift.id}>
+                    { shift.role_name },
+                    { moment(shift.start_time).format("hh:mm A") } -
+                    { moment(shift.end_time).format("hh:mm A") },
+                    { shift.user_name ? (
+                      <span>shift.user_name
+                        <button onClick={(e) => this.cancelOnClick(shift.id, e)}>CANCEL</button>
+                      </span>
+
+                    ) : (
+                      <span>OPEN
+                        <button onClick={(e) => this.cancelOnClick(shift.id, e)}>CLAIM</button>
+                      </span>
+                    )}
+                    { creator && (<button onClick={(e) => this.deleteOnClick(shift.id, e)}>DELETE</button>) }
+                  </li>
+                  )
+              })}
+            </ul>
+          </li>
+        </ul>
+      )
+    });
+
     return (
 
       <div>
-        {
-          !!allshifts.length ?
-            (
-              allshifts.map(shift => {
-                return (
-                  <ul>
-                    <li>Date
-                      <ul>
-                        <li key={shift.id}>
-                          {shift.role_name},
-                          {moment(shift.start_time).format("hh:mm A")} -
-                          {moment(shift.end_time).format("hh:mm A")},
-                          { !!shift.user_name ? (
-                            <span>shift.user_name
-                              <button onClick={(e) => this.cancelOnClick(shift.id, e)}>CANCEL</button>
-                            </span>
-
-                          ) : (
-                            <span>OPEN
-                              <button onClick={(e) => this.cancelOnClick(shift.id, e)}>CLAIM</button>
-                            </span>
-                          )}
-                          { !!creator ? (
-                          <button onClick={(e) => this.deleteOnClick(shift.id, e)}>DELETE</button>) :
-                          '' }
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                )
-              })
-            ) :
-          <div>Loading</div>
-        }
+        { /* allshifts.length === 0 ? <div>Loading</div> : shifts */ }
+        { shifts }
         {error && <div>{error}</div>}
       </div>
     );
   }
+
+
 }
