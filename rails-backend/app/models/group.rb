319ee4da784543b1035854b8e99364880b3211d7
Matt Latimer <matt.latimer@gmail.com>
2017-11-01 14:17:53 -0700

    fixes for deleting group

--- rails-backend/app/models/group.rb
+++ rails-backend/app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :group_members, dependent: :destroy
   has_many :users, through: :group_members
 
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :name, presence: true
 end
