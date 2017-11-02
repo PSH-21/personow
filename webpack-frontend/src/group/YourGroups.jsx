ff388b3c4df278938ba4d0d90d8b28a88175d6c6
Peter Hunt <peter.s.hunt@gmail.com>
2017-11-02 09:02:26 +0000

    add creator/member leave group links in user profile

--- webpack-frontend/src/group/YourGroups.jsx
+++ webpack-frontend/src/group/YourGroups.jsx
@@ -20,6 +20,9 @@ export default class YourGroups extends Component {
         {
           yourGroups.length === 0 ? <div>You have not created or are a part of any groups</div> :
             <table>
+              <thead>
+                <th></th>
+              </thead>
               <tbody>
                 {
                   yourGroups.map(group => {
@@ -27,6 +30,20 @@ export default class YourGroups extends Component {
                       <div key={group.id}>
                         <tr>
                           <td><Link to={`/groups/${group.id}`} params={{id: group.id}}>{group.name}</Link></td>
+                          <td>
+                            <div>
+                              { group.creator ? (
+                              <div>
+                                <span>  Creator </span>
+                                <span onClick={(e) => this.deleteOnClick(shift.id, e)}>Close Group</span>
+                              </div> ) : (
+                              <div>
+                                <span>  Member  </span>
+                                <span onClick={(e) => this.deleteOnClick(shift.id, e)}>Close Group</span>
+                              </div> )
+                              }
+                            </div>
+                          </td>
                         </tr>
                         <tr>
                           <td></td>
